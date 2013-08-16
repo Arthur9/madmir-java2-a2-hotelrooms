10643256363bac0acdf752c40441a0eaf33a8cd9
Arthur9 <jawzproductions@hotmail.com>
2013-08-16 12:08:49 -0400

    This is My Hotel Assignment Project Clyde. Thanks for your help!

--- src/HotelBilling.java
+++ src/HotelBilling.java
@@ -1,6 +1,10 @@
+import javax.swing.JOptionPane;
+
 
 public class HotelBilling {
 
+	final static double  SALES_TAX = 0.13f;
+	
 	public static void main(String[] args) {
 		/*
 		 * In the main method, declare and initialize variables
@@ -10,6 +14,63 @@ public class HotelBilling {
 		 * calculateBill() to demonstrate the use of overloaded 
 		 * methods
 		 */
+		
+		/*Declare value for objects*/
+		
+		double roomRate = 95.00;
+		double coupon = 0.10;
+		double totalBill;
+		int numRooms= Integer.parseInt(JOptionPane.showInputDialog("Enter the number of rooms:"));
+		
+		totalBill = computeBill(roomRate);		
+		JOptionPane.showMessageDialog(null,  "Your Total per room is $" + totalBill);
+		
+		
+		totalBill = computeBill(roomRate, numRooms);
+		JOptionPane.showMessageDialog(null, "Your total with tax for " + numRooms + " rooms is $" + totalBill);
+		
+		
+		totalBill = computeBill(roomRate, numRooms, coupon);
+		JOptionPane.showMessageDialog(null, "Your Total with tax for " + numRooms + " rooms(s) is $" + totalBill + ".\nThis Includes a discount of " + coupon * 100 + "%\n\nThanks for Checking us Out!" );
+		
+			
+	}
+	/* This is my piece of data for one piece of data
+	 * the room rate*/
+	
+	public static double computeBill(double rate){
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = twoDecimalRounding(finalRate);
+		return finalRate;
+	}
+	/*This is my piece of data for two pieces of data
+	 * the room rate and the quantity booked */
+	
+	public static double computeBill(double rate, int roomQuantity){
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = finalRate * roomQuantity;
+		finalRate = twoDecimalRounding(finalRate);
+		return finalRate;
+	}
+	/* This is a piece of data for three pieces of data*/
+	
+	public static double  computeBill (double rate, int roomQuantity, double coupon){
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = finalRate * roomQuantity * (1 -coupon);
+		finalRate = twoDecimalRounding(finalRate);
+		return finalRate;
+	}
+	/* This Method receives a double and returns the same double rounded to two decimal points*/
+	
+	public static double twoDecimalRounding(double decimal){
+		decimal = decimal*100;
+		decimal = decimal + 0.5;
+		decimal = (int) decimal;
+		decimal = decimal/100;
+		return decimal;
 	}
 
 	/*  
